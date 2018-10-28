3450bf9e36e8079dac471d6c368ef1b9c7303861
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-10-28 21:05:27 +1000

    fix

--- 01_basic.js
+++ 01_basic.js
@@ -10,15 +10,7 @@ const network = new brain.NeuralNetwork();
 //   { input: [1, 1, 1], output: [1] }
 // ]);
 
-network.train([
-  { input: [1, 2], output: [1] }, // Team 2 wins
-  { input: [1, 3], output: [1] }, // Team 3 wins
-  { input: [2, 3], output: [0] }, // Team 2 wins
-  { input: [2, 4], output: [1] }, // Team 4 wins
-  { input: [1, 2], output: [0] }, // Team 1 wins
-  { input: [1, 3], output: [0] }, // Team 1 wins
-  { input: [3, 4], output: [0] } // Team 3 wins
-]);
+
 
 const output = network.run([1, 4]);
 
